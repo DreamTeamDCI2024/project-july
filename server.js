fb560c360360b1224ce9c06d443b3946f476c915
Helen <iamhelena13@gmail.com>
2024-07-18 11:05:41 +0200

    start

---
+++ server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+// Initialize the app
+const app = express();
+
+// PORT
+const PORT = process.env.PORT || 4000;
+
+// Use the json middleware
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "50mb", extended: true }));
+
+// Paths
+app.get('/hello', (req, res) => {
+    console.log(req);
+
+    res.send("HI");
+})
+
+// Listening server
+mongoose
+    .connect(process.env.CONNECTION_URL)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server running on ${PORT}`);
+        });
+    })
+    .catch((error) => console.log(error));
+
+console.log('PORT:', process.env.PORT);
+console.log('CONNECTION_URL:', process.env.CONNECTION_URL);
