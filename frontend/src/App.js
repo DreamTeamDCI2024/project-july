4f296a37e89226f129eeae0cf205b1235200b9c4
Helen <iamhelena13@gmail.com>
2024-08-19 12:26:57 +0200

    added some empty pages, menu etc.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import Header from './Components/Header/Header';
-import Shop from './Pages/Shop';
+//import Shop from './Pages/Shop';
+import About from './Pages/About';
+import Products from './Pages/Products';
+import Contact from './Pages/Contact';
+import Login from './Pages/Login';
+import Cart from './Pages/Cart';
+import Homepage from './Pages/Homepage';
 
 function App() {
   return (
@@ -9,7 +15,13 @@ function App() {
       <BrowserRouter>
       <Header/>
       <Routes>
-        <Route path='/' element={<Shop/>}/>
+        <Route path='/' element={<Homepage/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/products' element={<Products/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+
       </Routes>
       </BrowserRouter>
     </div>
