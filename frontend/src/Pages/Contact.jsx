d62777ea07f29c937113d72aec59a3e914467a2d
Benjamin <benjamin.idamakin@dci-student.org>
2024-09-12 10:52:49 +0200

    fixed

--- frontend/src/Pages/Contact.jsx
+++ frontend/src/Pages/Contact.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import "./CSS/Contact.css"
-import mail_icon from "../Components/Assets/mail_icon.png"
-import location_icon from "../Components/Assets/location_icon.png"
-import msg_icon from "../Components/Assets/msg_icon.png"
-import phone_icon from "../Components/Assets/phone_icon.png"
+import "./CSS/Contact.css";
+import mail_icon from "../Components/Assets/mail_icon.png";
+import location_icon from "../Components/Assets/location_icon.png";
+import msg_icon from "../Components/Assets/msg_icon.png";
+import phone_icon from "../Components/Assets/phone_icon.png";
 
 const Contact = () => {
   const [result, setResult] = React.useState("");
@@ -17,7 +17,7 @@ const Contact = () => {
 
     const response = await fetch("https://api.web3forms.com/submit", {
       method: "POST",
-      body: formData
+      body: formData,
     });
 
     const data = await response.json();
@@ -33,28 +33,61 @@ const Contact = () => {
   return (
     <div className="contact">
       <div className="contact-col">
-        <h3>Send us a message <img src={msg_icon} alt="" /> </h3>
+        <h3>
+          Send us a message <img src={msg_icon} alt="" />{" "}
+        </h3>
         <p>
           Feel free to reach out through contact form or find our contact
           information below. Your feedback, questions, and suggestions are
-          important to us as we strive to provide exceptional service to our
-          university community.
+          important to us as we strive to provide exceptional service.
         </p>
         <ul>
-            <li><img src={mail_icon} alt="" />Contact@Bigos.dev</li>
-            <li> <img src={phone_icon} alt="" />+49 152-651-1321</li>
-            <li><img src={location_icon} alt="" />77 Ollenhaustrasse, Berlin<br /> 13401, Germany</li>
+          <li>
+            <img src={mail_icon} alt="" />
+            Contact@Bigos.dev
+          </li>
+          <li>
+            {" "}
+            <img src={phone_icon} alt="" />
+            +49 152-651-1321
+          </li>
+          <li>
+            <img src={location_icon} alt="" />
+            77 Ollenhaustrasse, Berlin
+            <br /> 13401, Germany
+          </li>
         </ul>
       </div>
       <div className="contact-col">
         <form onSubmit={onSubmit}>
-            <label>Your name</label> 
-            <input type="text" name="name" plac eholder="Enter your name" required/> <br />
-            <label>Phone Number</label>
-            <input type="tel" name="phone" placeholder="Enter your mobile" required/> <br />
-            <label>Write your messages here</label>
-            <textarea name="message" rows="6" placeholder="Enter your message" required></textarea> <br />
-            <button type="submit" className="btn dark-btn">Submit now</button>
+          <label>Your name</label>
+          <input
+            type="text"
+            name="name"
+            plac
+            eholder="Enter your name"
+            required
+          />{" "}
+          <br />
+          <label>Phone Number</label>
+          <input
+            type="tel"
+            name="phone"
+            placeholder="Enter your mobile"
+            required
+          />{" "}
+          <br />
+          <label>Write your messages here</label>
+          <textarea
+            name="message"
+            rows="6"
+            placeholder="Enter your message"
+            required
+          ></textarea>{" "}
+          <br />
+          <button type="submit" className="btn dark-btn">
+            Submit now
+          </button>
         </form>
         <span>{result}</span>
       </div>
@@ -63,7 +96,3 @@ const Contact = () => {
 };
 
 export default Contact;
-
-
-
-
